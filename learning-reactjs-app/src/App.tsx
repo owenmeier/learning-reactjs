d5f6b2023f9d5c0c7b3fd76e44bfe926650bf26f
Owen Meier <owen2717@gmail.com>
2025-05-28 13:51:18 -0400

    Created props functionality to easily reuse ListGroup function

--- learning-reactjs-app/src/App.tsx
+++ learning-reactjs-app/src/App.tsx
@@ -1,9 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let names = ["John", "Jane", "Doe", "Smith", "Alice"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={cities} heading="Cities" />
+      <ListGroup items={names} heading="Names" />
     </div>
   );
 }
