dc7f618488e29d407fce10a754fe6da7289dedff
Owen Meier <owen2717@gmail.com>
2025-05-28 13:26:19 -0400

    Added dynamic list items

--- learning-reactjs-app/src/components/ListGroup.tsx
+++ learning-reactjs-app/src/components/ListGroup.tsx
@@ -1,18 +1,16 @@
-
-
 function ListGroup() {
-    return (
-        <>
-            <h1>List</h1>
-            <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
-            </ul>
-        </>
-    );
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
